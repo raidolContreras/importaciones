da96f757357cfdf5dd47a029308fa8d9f07dfe68
Sebastian-Landin-AIE <sebastian.landinsolorio@asesoria-integral-empresarial.com>
2025-04-29 20:41:54 -0600

    prueva

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9c33500ff09ebc1ac3193f031e37ad121a2c39ca',
+        'reference' => 'e6519b8200a836d50ee0ca0089fef321d4bf5269',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9c33500ff09ebc1ac3193f031e37ad121a2c39ca',
+            'reference' => 'e6519b8200a836d50ee0ca0089fef321d4bf5269',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
